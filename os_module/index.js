fe3aa88380227869c087f25a54c52ab39119d6ec
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-23 18:54:36 +0530

    done with os_module

---
+++ os_module/index.js
@@ -0,0 +1,22 @@
+
+
+const os = require('os');
+
+// to get free system memory 
+
+console.log(os.freemem());
+
+// to get name of home directory
+
+console.log(os.homedir())
+
+// to get hostname 
+
+console.log(os.hostname())
+
+// to check which machine user is working on
+
+console.log(os.machine())
+
+//
+console.log(os.networkInterfaces())
