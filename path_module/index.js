125a84a28335571029de97ffb8277615497909dc
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-23 15:50:43 +0530

    done with path module

---
+++ path_module/index.js
@@ -0,0 +1,11 @@
+const path = require('path');
+
+
+// to get base name of path
+const a = path.basename('C:\Users\Dell\Desktop\nodejs-starting\module\index.js')
+const b = path.dirname('C:\Users\Dell\Desktop\nodejs-starting\module\index.js')
+const c = path.extname('C:\Users\Dell\Desktop\nodejs-starting\module\index.js')
+
+console.log(a)
+console.log(b)
+console.log(c)
