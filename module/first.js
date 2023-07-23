5d3ed9d7c863fc9ebac22415c9580b1cec4db315
ganeshjadhav2452 <ganeshjadhav2452@gmail.com>
2023-07-23 14:43:58 +0530

    done with modules

---
+++ module/first.js
@@ -0,0 +1,11 @@
+
+const multiply = (a,b)=>{
+    return a * b
+}
+
+
+const concat = (a,b)=>{
+    return a + b
+}
+
+module.exports = {multiply,concat}
---
+++ module/index.js
@@ -0,0 +1,5 @@
+// importing modules
+
+const {concat, multiply} = require('./first')
+
+console.log(concat('ganesh ', 'jadhav'),multiply(5,10))
